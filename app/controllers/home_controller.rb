c6bec5aa179e94e14dad120e294479103d80db8f
Alexander Frankel <alexxander.frankel@gmail.com>
2014-10-18 14:16:30 -0400

    Implement accessing User Google Calendar functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
 	def index
+		if current_user
+		  client = Google::APIClient.new
+		  client.authorization.access_token = current_user.oauth_token
+		  service = client.discovered_api('calendar', 'v3')
+		  options = {
+		    :api_method => service.calendar_list.list,
+		    :parameters => {},
+		    :headers    => {"Content-Type" => "application/json"}
+		  }
+		  @result = client.execute(options)
+		end
 	end
 end
