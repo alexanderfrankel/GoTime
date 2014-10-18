b640a13c7a6e0b53170cb7c3c6926d9ea7210ede
Alexander Frankel <alexxander.frankel@gmail.com>
2014-10-18 14:02:50 -0400

    Add User email in new user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
 	def self.from_omniauth(auth)
 		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+			user.email = auth.info.email
 			user.provider = auth.provider
 			user.uid = auth.uid
 			user.oauth_token = auth.credentials.token
