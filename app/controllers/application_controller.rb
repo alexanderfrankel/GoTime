871c463bee49879008d64ecd99281c9bbeed302f
Alexander Frankel <alexxander.frankel@gmail.com>
2014-10-19 15:44:32 -0400

    Format homepage and add user authorized field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :user_authorized?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_authorized?
+  	if current_user
+  		return true if current_user.authorized?
+  	end
+  	return false
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-	after_create :build_events
-
 	has_many :events
 	has_many :locations
 
@@ -13,10 +11,5 @@ class User < ActiveRecord::Base
 			user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 			user.save!
 		end
-	end
-
-	def build_events
-		event_builder = EventBuilder.new(self)
-		event_builder.add_appts_and_transit_events_to_database
 	end
 end
