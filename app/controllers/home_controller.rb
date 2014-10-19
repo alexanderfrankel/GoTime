5652209d435e9df0aac0578a5056bf916f4a2e63
Alexander Frankel <alexxander.frankel@gmail.com>
2014-10-18 21:45:39 -0400

    Still testing oauth funtionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
 	def index
 		if current_user
-		  @user_calendar = GoogleCalendar.new(current_user)
-		  @result = user_calendar.display_calendar_events
+			@current_user = current_user
+		  # user_calendar = GoogleCalendar.new(current_user)
+		  # @result = user_calendar.display_calendar_events
 		end
 	end
 end
