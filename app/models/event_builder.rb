de1686273164b917ff2f3a85caef82e3b52d8292
Zach Karst <zach@karst.co>
2014-10-19 18:53:17 -0400

    Update directionsquery references
    -Location now in user and event records
    -Correct time formating
    -Add event time argument to create calendar event method

--- app/models/event_builder.rb
+++ app/models/event_builder.rb
@@ -6,19 +6,19 @@ class EventBuilder
 
 	def add_appts_and_transit_events_to_database
 		retrieve_events_with_location.each do |appt_event|
-			
-		event = Event.new
-		event.dest_loc = save_dest_location
-		event.appt_id = appt_event.id
-		event.user = @user
-		event.save
 
-		transit_directions = get_transit_directions(event.orig_loc, event.dest_loc, format_event_time(appt_event))
+			event = Event.new
+			event.location = Location.create(address: appt_event.location)
+			event.appt_id = appt_event.id
+			event.user = @user
+			event.save
 
-		transit_event = add_transit_event_to_calendar(appt_event, transit_directions, @parsed_time)
+			transit_directions = get_transit_directions(@user.location, event.location, appt_event.start["dateTime"])
 
-		event.transit_id = transit_event.id
-		event.save
+			transit_event = add_transit_event_to_calendar(appt_event, transit_directions, @parsed_time)
+
+			event.transit_id = transit_event.id
+			event.save
 		end
 		save_sync_token
 	end
@@ -29,8 +29,8 @@ class EventBuilder
 		@user_calendar.retrieve_calendar_events_with_location
 	end
 
-	def add_transit_event_to_calendar(appt_event)
-		@user_calendar.add_transit_event(appt_event)
+	def add_transit_event_to_calendar(appt_event, transit_directions, parsed_time)
+		@user_calendar.add_transit_event(appt_event, transit_directions, parsed_time)
 	end
 
 	def save_sync_token
@@ -38,16 +38,12 @@ class EventBuilder
 		@user.save
 	end
 
-	def format_event_time(appt_event)
-		DateTime.rfc3339(appt_event.start["dateTime"])
-	end
-
-	def save_dest_location(appt_event)
-		Location.create(appt_event.location)
-	end
+	# def format_event_time(appt_event)
+	# 	DateTime.rfc3339(appt_event.start["dateTime"])
+	# end
 
 	def get_transit_directions(orig_loc, dest_loc, event_time)
-		directions = GoogleDirectionQuery.new(orig_loc, dest_loc, event_time)
+		directions = GoogleDirectionQuery.new(orig_loc, dest_loc, event_time).get_directions
 		parser = GoogleDirectionParser.new(directions)
 		@parsed_time = parser.parse_time
 		parser.parse_directions
